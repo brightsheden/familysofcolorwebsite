ef150e2fa060dfd8a283320da3c5d96afa120175
shedenbright <shedenbright@gmail.com>
2023-12-14 22:01:03 -1200

    ready to take off

---
+++ backend/urls.py
@@ -0,0 +1,19 @@
+
+
+from django.contrib import admin
+from django.urls import  path,include
+from django.conf import settings
+from django.conf.urls.static import  static
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('base.urls')),
+    #path('support/', include('live_support.urls')),
+   # path('egg_record/', include('base.urls.egg_production'))
+]
+
+
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
